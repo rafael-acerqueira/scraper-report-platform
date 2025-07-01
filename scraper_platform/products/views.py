6fd0ac864853074c04a019083132398733a80286
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-07-01 09:54:26 -0300

    Add filters to products endpoints

--- scraper_platform/products/views.py
+++ scraper_platform/products/views.py
@@ -1,14 +1,18 @@
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
+
+from .filters import ProductFilter
 from .models import Product
 from .serializers import ProductSerializer
 
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all().order_by('-collected_at')
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields =  ['title', 'price', 'collected_at']
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = ProductFilter
     search_fields = ['title']
+    ordering_fields = ['price', 'collected_at']
+    ordering = ['-collected_at']
 
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
