c393d8ee7d7960d7d9c5c055fad39f74c694e03d
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-07-02 10:42:19 -0300

    Export data from databse to csv/json file

--- scraper_platform/products/urls.py
+++ scraper_platform/products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductExportView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail')
+    path('products/export/', ProductExportView.as_view(), name='product-export'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+
 ]
--- scraper_platform/products/views.py
+++ scraper_platform/products/views.py
@@ -1,5 +1,9 @@
-from rest_framework import generics, filters
+import csv
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, filters, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .filters import ProductFilter
 from .models import Product
@@ -17,3 +21,26 @@ class ProductListView(generics.ListAPIView):
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductExportView(APIView):
+    def get(self, request):
+        export_format = request.query_params.get('export_format', 'json')
+        queryset = Product.objects.all()
+        filterset = ProductFilter(request.GET, queryset=queryset)
+        products = filterset.qs
+
+        serializer = ProductSerializer(products, many=True)
+        data = serializer.data
+
+        if export_format == 'csv':
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="products.csv"'
+            writer = csv.writer(response)
+            writer.writerow(['id', 'title', 'price', 'collected_at'])
+            for item in data:
+                writer.writerow([item['id'], item['title'], item['price'], item['collected_at']])
+            return response
+
+
+        return Response(data, status=status.HTTP_200_OK)
