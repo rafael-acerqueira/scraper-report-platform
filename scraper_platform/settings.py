595055b3168c049432cfcc4833c38795eb74f9a7
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-07-04 19:39:21 -0300

    Customize Admin title, header, add products filter, ordening and actions

--- scraper_platform/settings.py
+++ scraper_platform/settings.py
@@ -3,3 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 EXPORT_BUCKET_PATH = os.path.join(BASE_DIR, "s3_bucket")
+
+ADMIN_SITE_HEADER = "Scraper Report Platform Admin"
+ADMIN_SITE_TITLE = "Scraper Admin"
+ADMIN_INDEX_TITLE = "Welcome to Scraper Painel"
